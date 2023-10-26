f47841bc6847ecf694ccd298511dd579d23b68ce
vbreksidler <vbreksidler@gmail.com>
2023-10-26 11:51:14 -0300

    uploading-sfcc-site

---
+++ cartridges/app_storefront_custom/cartridge/controllers/Training.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var server = require("server");
+var csrfProtection = require("*/cartridge/scripts/middleware/csrf");
+var consentTracking = require("*/cartridge/scripts/middleware/consentTracking");
+
+server.get("Show", consentTracking.consent, server.middleware.https, csrfProtection.generateToken, function (
+    req,
+    res,
+    next
+) {
+    var URLUtils = require("dw/web/URLUtils");
+    var Resource = require("dw/web/Resource");
+
+    var profileForm = server.forms.getForm("training");
+    profileForm.clear();
+
+    res.render("trainingform", {
+        title: Resource.msg("training.form.title.submit", "forms", null),
+        profileForm: profileForm,
+        actionUrl: URLUtils.url("Training-SubmitRegistration").toString()
+    });
+
+    next();
+});
+
+server.post(
+    "SubmitRegistration",
+    server.middleware.https,
+    consentTracking.consent,
+    csrfProtection.generateToken,
+    function (req, res, next) {
+        var Resource = require("dw/web/Resource");
+        var URLUtils = require("dw/web/URLUtils");
+        var profileForm = server.forms.getForm("training");
+        res.render("trainingform", {
+            title: Resource.msg("training.form.title.edit", "forms", null),
+            profileForm: profileForm,
+            actionUrl: URLUtils.url("Training-SubmitRegistration").toString()
+        });
+
+        next();
+    }
+);
+
+module.exports = server.exports();
