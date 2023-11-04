024db55e202c6635d2b4a51e56a5664e91e9c2cd
vbreksidler <vbreksidler@gmail.com>
2023-11-04 08:13:10 -0300

    feat - add coupon generate, email with coupon code, email if there is no coupon, error email already registred

--- cartridges/app_storefront_custom/cartridge/controllers/Newsletter.js
+++ cartridges/app_storefront_custom/cartridge/controllers/Newsletter.js
@@ -24,32 +24,46 @@ server.post('Subscribe', server.middleware.https, function (req, res, next) {
         hooksHelper('app.newsletter.subscribe', 'subscribe', newsletterDetails, function () { });
 
         server.append('Subscribe', function (req, res, next) {
-            var emailHelpers = require('*/cartridge/scripts/helpers/emailHelpers')
+            var CouponMgr = require('dw/campaign/CouponMgr');
             var fromEmail = dw.system.Site.getCurrent().getPreferences().custom.customerServiceEmail;
-            // var Coupon = require('dw/campaign/Coupon');
-            // var Promotion = require('dw/campaign/Promotion');
-            // var Campaign = require('dw/campaign/Campaign');
-            // var Transaction = require('dw/system/Transaction');
-            // Transaction.wrap(function(){
-            // })
-            //learn abou Transaction
-            
-            var objectForEmail = {
-                firstName: myForm.newsletterFirstName,
-                lastName: myForm.newsletterLastName,
-            };
+            var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+            var Transaction = require('dw/system/Transaction');
 
-            var emailObj = {
-                to: myForm.newsletterEmail,
-                subject: Resource.msg('subject.confimation.email', 'newsletter', null),
-                from: fromEmail,
-                type: emailHelpers.emailTypes.newsletter
-            };
-            emailHelpers.sendEmail(emailObj, '/mail/newsletter', objectForEmail);
+            Transaction.wrap(function () {
+                if (CustomObjectMgr.getCustomObject("NewsletterSubscription", myForm.newsletterEmail)) {
+                    throw new Error('Sorry, this e-mail is already registred for Newsletter');
+                }
+                CustomObjectMgr.createCustomObject("NewsletterSubscription", myForm.newsletterEmail);
+                var coupons = CouponMgr.getCoupons();
+                var couponCode = coupons[0].nextCouponCode;
+    
+                var objectForEmail = {
+                    firstName: myForm.newsletterFirstName,
+                    lastName: myForm.newsletterLastName,
+                    couponCode,
+                };
 
+                if (couponCode === null) {
+                    var emailObj = {
+                        to: myForm.newsletterEmail,
+                        subject: Resource.msg('subject.confimation.email', 'newsletter', null),
+                        from: fromEmail,
+                        type: emailHelper.emailTypes.newsletter
+                    };
+                    emailHelper.sendEmail(emailObj, '/mail/newsletterError', objectForEmail);
+                } else {
+                    var emailObj = {
+                        to: myForm.newsletterEmail,
+                        subject: Resource.msg('subject.confimation.email', 'newsletter', null),
+                        from: fromEmail,
+                        type: emailHelper.emailTypes.newsletter
+                    };
+                    emailHelper.sendEmail(emailObj, '/mail/newsletter', objectForEmail);
+                    }
+                })
+                
             next();
         })
-
         res.json({
             success: true,
             msg: Resource.msg('subscribe.to.newsletter.success', 'newsletter', null)
